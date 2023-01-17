e9feb660ee3706a1eeb4c90f1e33c56d19d7e37d
01hevel <01hevel@gmail.com>
2023-01-17 15:48:55 +0545

    Interface implemented

---
+++ app/src/main/java/com/example/estateempires/AppInterface.java
@@ -0,0 +1,15 @@
+package com.example.estateempires;
+
+public interface AppInterface {
+    public interface wholeApp {
+        public void login();
+        public void signup();
+        public void profileSetup();
+        public void listProperty();
+        public void rentPropertiesListing();
+        public void gigsListing();
+        public void like();
+        public void follow();
+        public void chat();
+    }
+}
