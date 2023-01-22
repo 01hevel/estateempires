b0ae8bf3ee81c0d7c38565e0dea452074f318b9d
01hevel <01hevel@gmail.com>
2023-01-22 08:49:11 +0545

    f

---
+++ app/src/main/java/com/example/estateempires/login.java
@@ -0,0 +1,9 @@
+package com.example.estateempires;
+
+
+class agentLogin extends login{
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ app/src/main/java/com/example/estateempires/propertyListing.java
@@ -0,0 +1,13 @@
+package com.example.estateempires;
+
+public interface propertyListing {
+    public void listProperty();
+
+    public void rentPropertiesListing();
+
+    public void gigsListing();
+
+    public void like();
+
+    public void follow();
+}
