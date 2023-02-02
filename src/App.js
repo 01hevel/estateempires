ec9e5a5e4a6038e4325bfa57f136cc92d6cde855
01hevel <01hevel@gmail.com>
2023-02-02 20:38:55 +0545

    lets tweak UI

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing, Register, Error, ProtectedRoute } from './pages';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AddJob, AllJobs, Profile, SharedLayout, Stats } from './pages/dashboard';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={
+          <ProtectedRoute>
+            <SharedLayout />
+          </ProtectedRoute>
+        } >
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
+        <Route path='landing' element={<Landing />} />
+        <Route path='register' element={<Register />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <ToastContainer position='top-center' autoClose={1000} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
